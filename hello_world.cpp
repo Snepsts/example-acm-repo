f1358bf9486161a297eea0cee8bd3805ab3586ef
Michael Ranciglio <Snepsts@users.noreply.github.com>
2018-01-28 23:35:37 -0600

    Add "add" function
    This function will take two ints and return their sum.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+int add (int x, int y);
+
 int main()
 {
 	int a = 5;
@@ -8,4 +10,9 @@ int main()
 	std::cout << "a = " << a << '\n';
 
 	return 0;
+}
+
+int add (int x, int y)
+{
+	return x + y;
 }
