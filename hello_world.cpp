eaf3499a46d8bbdc3d311823bdde9d790cfad526
Michael Ranciglio <rancigliom@gmail.com>
2018-01-28 23:39:00 -0600

    Add "multiply" function
    This function will take two ints and return their product.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+int multiply(int x, int y);
+
 int main()
 {
 	int a = 5;
@@ -8,4 +10,9 @@ int main()
 	std::cout << "a = " << a << '\n';
 
 	return 0;
+}
+
+int multiply(int x, int y)
+{
+	return x * y;
 }
